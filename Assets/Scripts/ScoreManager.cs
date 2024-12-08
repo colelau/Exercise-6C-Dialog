7041998a1bb86a3c9d3db8b42aad5eb7b4037b50
colelau <colelau@iu.edu>
2024-12-07 21:32:37 -0500

    Made coinscript and managerscript

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager sManager;
+    public int playerScore = 0;
+
+
+    void Start()
+    {
+        sManager = this;  
+    }
+
+    public void IncreaseScore(int Increase)
+    {
+        playerScore += Increase;
+
+    }
+}
